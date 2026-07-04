fa977a71497df1d7dd281f3e37b7a918f065ba7c
Ashiqur Zaman Arshad <realershad67@gmail.com>
2026-07-04 22:40:59 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 // Xperiencer Service Worker
-const CACHE_NAME = 'xperiencer-v1';
+const CACHE_NAME = 'xperiencer-v2';
 const PRECACHE = [
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
